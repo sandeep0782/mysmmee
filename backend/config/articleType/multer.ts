0069cafe6cc61e242f6168aeae0f0b48f6c8bc5b
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2026-01-15 13:28:53 +0530

    at-4

--- backend/config/articleType/multer.ts
+++ backend/config/articleType/multer.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
+  limits: { fileSize: 500 * 1024 * 1024 }, // 5MB max
 });
 
 export default upload;
