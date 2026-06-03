e32aba442cd0b72959a9465a90763134422940b8
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2026-06-04 01:12:23 +0530

    fix robots.txt remove Host directive

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   siteUrl: "https://mysmme.com",
   generateRobotsTxt: true,
+
+  exclude: [
+    "/admin",
+    "/admin/**",
+
+    "/account",
+    "/account/**",
+
+    "/checkout",
+    "/checkout/**",
+  ],
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/account", "/checkout"],
+      },
+    ],
+  },
 };
